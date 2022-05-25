61991a674e231d354047b78119390bd492ff3e6c
lsepu <55538804+lsepu@users.noreply.github.com>
2022-05-25 16:46:49 -0500

    ADD: kata 9 solved

--- src/main/java/katas/Kata4.java
+++ src/main/java/katas/Kata4.java
@@ -8,6 +8,7 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /*
@@ -19,9 +20,14 @@ public class Kata4 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
+        //needs to be 150 x 200
+        Function<List<BoxArt>, String> getBoxArtUrl = (boxarts) -> boxarts.stream()
+                .filter(boxArt -> boxArt.getWidth()==150 && boxArt.getHeight()==200)
+                .map(box -> box.getUrl()).findFirst().get();
+
         return movieLists.stream().map(movie -> movie.getVideos())
                 .flatMap(videos -> videos.stream())
-                .map(video -> Map.of("id",video.getId(),"title",video.getTitle(),"boxart",video.getBoxarts()))
+                .map(video -> Map.of("id",video.getId(),"title",video.getTitle(),"boxart",getBoxArtUrl.apply(video.getBoxarts())))
                 .collect(Collectors.toList());
 
         //return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", new BoxArt(150, 200, "url")));
--- src/main/java/katas/Kata9.java
+++ src/main/java/katas/Kata9.java
@@ -3,6 +3,7 @@ package katas;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import model.Bookmark;
+import model.InterestingMoment;
 import model.Movie;
 import model.MovieList;
 import util.DataUtil;
@@ -10,6 +11,8 @@ import util.DataUtil;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve each video's id, title, middle interesting moment time, and smallest box art url
@@ -20,6 +23,16 @@ public class Kata9 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "some title", "time", new Date(), "url", "someUrl"));
+        Function<List<InterestingMoment>, Date> getMiddleInterestingMoment = (interestingMoments) ->
+                interestingMoments.stream().filter(interestingMoment -> interestingMoment.getType().equals("Middle"))
+                        .map(interestingMoment -> interestingMoment.getTime()).findFirst().get();
+
+        return movieLists.stream().map(movie -> movie.getVideos())
+                .flatMap(videos -> videos.stream())
+                .map(video -> Map.of("id", video.getId(), "title", video.getTitle(),
+                        "time", getMiddleInterestingMoment.apply(video.getInterestingMoments()),
+                        "url", video.getUri()))
+                .collect(Collectors.toList());
+
     }
 }
