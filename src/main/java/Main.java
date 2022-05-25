63e14894b51d02c253b7848c69a866cb4ffa8751
lsepu <55538804+lsepu@users.noreply.github.com>
2022-05-25 15:33:01 -0500

    ADD: kata 4 solved

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,17 +7,16 @@ public class Main {
         assertEquals(Kata1.execute().size(), 4);
         System.out.println("Kata 1 solved!");
 
-
         assertEquals(Kata2.execute().size(), 2);
         System.out.println("Kata 2 solved!");
 
         assertEquals(Kata3.execute().size(), 4);
         System.out.println("Kata 3 solved!");
 
-        /*
-
         assertEquals(Kata4.execute().size(), 4);
         System.out.println("Kata 4 solved!");
+
+        /*
 
         assertEquals(Kata5.execute(), 5.0, 0.0);
         System.out.println("Kata 5 solved!");
